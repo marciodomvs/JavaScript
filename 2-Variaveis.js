c07ad9a08e3159b5918be6ef6aee575c7e1d6cc7
Márcio Silva <marcio.guilherme@domvsit.com.br>
2021-07-28 19:40:55 -0300

    upando aula 2 e 3

---
+++ 2-Variaveis.js
@@ -0,0 +1,9 @@
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+idade = 29; //variavel global
+
+const idade = 29; //variavel local
+const nome = "márcio";
+
+//não fazer -> mes = "Janeiro";
