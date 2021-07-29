bc624e8a205a410e6ca9eb3d22ec4263ff6ba542
Márcio Silva <marcio.guilherme@domvsit.com.br>
2021-07-29 09:45:53 -0300

    adicionando aula 4

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,15 @@
+console.log("Conversão de Tipos");
+
+console.log("ano de " + 2020);
+
+console.log("2" + "2");
+
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("7" / "2"); //trabalha com string
+
+console.log("Ricardo" / "2"); //NaN -> Not a Number
+
+console.log(6.5); //certo
+
+console.log(6,5); //errado
