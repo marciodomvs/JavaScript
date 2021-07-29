7ede7ef6410d337f5ee64c8cc090699df079c075
Márcio Silva <marcio.guilherme@domvsit.com.br>
2021-07-29 11:34:19 -0300

    upando atualização da aula 7

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -2,20 +2,19 @@ console.log(`Trabalhando com condicionais`);
 
 const listaDeDestinos = new Array("Salvador","São Paulo","Rio de Janeiro", "Curitiba");
 const idadeComprador = 17;
-console.log(`Destinos possíveis: ${listaDeDestinos}`);
 const estaAcompanhada = true;
+let temPassagemComprada = false;
 
-if(idadeComprador >= 18){ //se comprador é maior de idade, libera para compra e tira da lista
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1,1);
-}else{
-    console.log("Comprador não é maior de idade"); //pessoa é menor de idade
-    if(estaAcompanhada){
-        console.log("Comprador maior de idade");
-        listaDeDestinos.splice(1,1);
-    }else console.log("Não está acompanhado");
-}
 console.log(`Destinos possíveis: ${listaDeDestinos}`);
+if(idadeComprador >= 18 || estaAcompanhada){ //se comprador é maior de idade, libera para compra e tira da lista
+    temPassagemComprada = true;
+    console.log("Comprador é maior de idade ou está acompanhado");
+    listaDeDestinos.splice(2,1);
+}else console.log("Comprador não é maior de idade e não está acompanhado\n"); //pessoa é menor de idade
+
+console.log("Embarque: \n\n");
+if(temPassagemComprada) console.log("Boa viagem!!!")
+else console.log("Você não pode embarcar");
 
 console.log(idadeComprador < 18);
 console.log(idadeComprador > 18);
