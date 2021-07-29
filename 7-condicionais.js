866ff35823af166b5bd166bf3e751a42021b1ef8
Márcio Silva <marcio.guilherme@domvsit.com.br>
2021-07-29 11:21:14 -0300

    upando aula 7

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -1,16 +1,25 @@
-console.log(`Trabalhando com listas`);
+console.log(`Trabalhando com condicionais`);
 
-const salvador = `Salvador`;
-const saoPaulo = `São Paulo`;
-const rioDeJaneiro = `Rio de Janeiro`;
-console.log(`Destinos possíveis: ${salvador}, ${saoPaulo}, ${rioDeJaneiro}`);
-//prática ruim
-//ctrl + kc = comenta tudo selecionado; ctrl + ku = descomenta tudo selecionado
+const listaDeDestinos = new Array("Salvador","São Paulo","Rio de Janeiro", "Curitiba");
+const idadeComprador = 17;
+console.log(`Destinos possíveis: ${listaDeDestinos}`);
+const estaAcompanhada = true;
 
-const listaDeDestinos = new Array("Salvador","São Paulo","Rio de Janeiro"); //new é uma palavra reservada
-listaDeDestinos.push("Curitiba"); //adicionando um item na lista, da pra fazer push em variaveis const
+if(idadeComprador >= 18){ //se comprador é maior de idade, libera para compra e tira da lista
+    console.log("Comprador maior de idade");
+    listaDeDestinos.splice(1,1);
+}else{
+    console.log("Comprador não é maior de idade"); //pessoa é menor de idade
+    if(estaAcompanhada){
+        console.log("Comprador maior de idade");
+        listaDeDestinos.splice(1,1);
+    }else console.log("Não está acompanhado");
+}
 console.log(`Destinos possíveis: ${listaDeDestinos}`);
 
-listaDeDestinos.splice(1,1); //deleta elemento da lista -> (referencia do elemento, numero de elementos que vai ser excluido a partir da posição de referencia)
-console.log(`Destinos possíveis: ${listaDeDestinos}`);
-console.log(listaDeDestinos[1], listaDeDestinos[0]); //printando posição especifica da lista
+console.log(idadeComprador < 18);
+console.log(idadeComprador > 18);
+console.log(idadeComprador <= 18);
+console.log(idadeComprador >= 18);
+console.log(idadeComprador == 18);
+//operadores lógicos, retorn is boolean (true or false)
